47ce2fdfac955baf41457b7b0a8ae785e22c4721
jorgesanchezperez <jorge.sanchez-perez@forgerock.com>
2022-02-18 13:17:36 +0000

    Added condition on Oauth2 exist methods

--- am/oauth2.go
+++ am/oauth2.go
@@ -96,12 +96,14 @@ func CreateRemoteConsentService() {
 func RemoteConsentExists(name string) bool {
 	path := "/am/json/realms/root/realms/alpha/realm-config/agents/RemoteConsentAgent?_queryFilter=true&_pageSize=10&_fields=agentgroup"
 	consent := &AmResult{}
-	b, _ := Client.Get(path, map[string]string{
+	b, status := Client.Get(path, map[string]string{
 		"Accept":             "application/json",
 		"X-Requested-With":   "ForgeRock Identity Cloud Postman Collection",
 		"Accept-Api-Version": "protocol=2.0,resource=1.0",
 	})
-
+	if status == http.StatusNotFound {
+		return false
+	}
 	err := json.Unmarshal(b, consent)
 	if err != nil {
 		panic(err)
@@ -209,12 +211,14 @@ func CreateSoftwarePublisherAgentTestPublisher() {
 func SoftwarePublisherAgentExists(name string) bool {
 	path := "/am/json/realms/root/realms/alpha/realm-config/agents/SoftwarePublisher?_queryFilter=true&_pageSize=10&_fields=agentgroup"
 	agent := &AmResult{}
-	b, _ := Client.Get(path, map[string]string{
+	b, status := Client.Get(path, map[string]string{
 		"Accept":             "application/json",
 		"X-Requested-With":   "ForgeRock Identity Cloud Postman Collection",
 		"Accept-Api-Version": "protocol=2.0,resource=1.0",
 	})
-
+	if status == http.StatusNotFound {
+		return false
+	}
 	err := json.Unmarshal(b, agent)
 	if err != nil {
 		panic(err)
@@ -308,12 +312,14 @@ func UpdateOAuth2Provider(claimsScriptID string) {
 func Oauth2ProviderExists(id string) bool {
 	path := "/am/json/realms/root/realms/alpha/realm-config/services?_queryFilter=true"
 	r := &AmResult{}
-	b, _ := Client.Get(path, map[string]string{
+	b, status := Client.Get(path, map[string]string{
 		"Accept":             "application/json",
 		"X-Requested-With":   "ForgeRock Identity Cloud Postman Collection",
 		"Accept-Api-Version": "protocol=1.0,resource=1.0",
 	})
-
+	if status == http.StatusNotFound {
+		return false
+	}
 	err := json.Unmarshal(b, r)
 	if err != nil {
 		panic(err)
