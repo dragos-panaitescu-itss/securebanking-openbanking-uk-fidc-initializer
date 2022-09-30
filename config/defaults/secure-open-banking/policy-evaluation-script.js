23fa34ee9a2b91597535b49d39df57728da066f4
Jorge Sanchez Perez <54277573+jorgesanchezperez@users.noreply.github.com>
2022-09-30 08:39:21 +0100

    468: update policy script to support international standing orders (#108)
    * 468: update policy script to support international standing orders
    - Policy updated to support international standing orders
    Issue: https://github.com/securebankingaccesstoolkit/securebankingaccesstoolkit/issues/468
    
    * fix variables definition compatibility javascript support

--- config/defaults/secure-open-banking/policy-evaluation-script.js
+++ config/defaults/secure-open-banking/policy-evaluation-script.js
@@ -40,7 +40,10 @@ var accountsAndTransactionsPermissions = [
     {name: "READTRANSACTIONSDETAIL", property: {permission: "ReadTransactionsDetail", requestType: "transactions"}}
 ];
 
-var paymentsIntents = ["domesticPaymentIntent", "domesticScheduledPaymentIntent", "domesticStandingOrderIntent", "internationalPaymentIntent", "internationalScheduledPaymentIntent"];
+var paymentsIntents = [
+    "domesticPaymentIntent", "domesticScheduledPaymentIntent", "domesticStandingOrderIntent",
+    "internationalPaymentIntent", "internationalScheduledPaymentIntent", "internationalStandingOrderIntent"
+];
 
 function getPermissionAccountAndTransactions(name) {
     for (var i = 0; i < accountsAndTransactionsPermissions.length; i++) {
@@ -205,6 +208,8 @@ function findIntentType(api) {
         return "internationalPaymentIntent"
     } else if (api === "international-scheduled-payments" || api === "international-scheduled-payment-consents") {
         return "internationalScheduledPaymentIntent"
+    } else if (api == "international-standing-orders" || api == "international-standing-order-consents") {
+        return "internationalStandingOrderIntent"
     }
     return null
 }
