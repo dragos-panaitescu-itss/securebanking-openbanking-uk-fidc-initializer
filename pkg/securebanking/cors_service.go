545d505e9c365786c3e052655a7f2285622c71ef
Dan Badham <106093201+dbadham-fr@users.noreply.github.com>
2022-11-03 17:10:40 +0000

    629 configure cors for login UI (#127)
    * 629: Configuring CORS Service in AM
    
    Adding cors-login-ui.json configuration file for AM CORS Service and cors_service.go which uses the AM REST API to apply the config.
    
    The CORS configuration is required because the login-ui will be accessed via IG going forwards, therefore the origin of the request will be IG rather than AM. This is required for FAPI compliance.
    
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/629
    
    * 629: Call go clean from Makefile
    
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/629
    
    * 629: Configuring CORS Service in AM
    
    Adding cors-login-ui.json configuration file for AM CORS Service and cors_service.go which uses the AM REST API to apply the config.
    
    The CORS configuration is required because the login-ui will be accessed via IG going forwards, therefore the origin of the request will be IG rather than AM. This is required for FAPI compliance.
    
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/629
    
    * 629: Call go clean from Makefile
    
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/629
    
    * 629: Enabling github action for a PR targeting any branch
    
    Issue: https://github.com/SecureBankingAccessToolkit/SecureBankingAccessToolkit/issues/629

--- pkg/securebanking/cors_service.go
+++ pkg/securebanking/cors_service.go
@@ -9,7 +9,35 @@ import (
 
 func ConfigureAmCorsService(cookie *http.Cookie) {
 	zap.L().Info("Configuring CORS")
+<<<<<<< HEAD
 	CreateCrestResourceFromConfigFile(fmt.Sprintf("https://%s/am/json/global-config/services/CorsService/configuration?_action=create",
 		common.Config.Hosts.IdentityPlatformFQDN),
 		"cors-login-ui.json", cookie)
+=======
+
+	var corsConfig map[string]interface{}
+	common.Unmarshal(common.Config.Environment.Paths.ConfigSecureBanking+"cors-login-ui.json", &common.Config, &corsConfig)
+	path := fmt.Sprintf("https://%s/am/json/global-config/services/CorsService/configuration?_action=create", common.Config.Hosts.IdentityPlatformFQDN)
+
+	var responsePayload map[string]interface{}
+	resp, err := restClient.R().
+		SetHeader("Accept", "*/*").
+		SetHeader("Content-Type", "application/json").
+		SetHeader("Connection", "keep-alive").
+		SetHeader("X-Requested-With", "XMLHttpRequest").
+		SetHeader("Accept-API-Version", "protocol=1.0,resource=1.0").
+		SetContentLength(true).
+		SetCookie(cookie).
+		SetBody(corsConfig).
+		SetResult(&responsePayload).
+		Post(path)
+
+	zap.S().Info("resp is " + resp.String())
+	if resp != nil && resp.StatusCode() == 409 {
+		zap.S().Info("CORS Service configuration already exists")
+	} else {
+		common.RaiseForStatus(err, resp.Error(), resp.StatusCode())
+		zap.S().Info("Created CORS Service configuration, _id: ", responsePayload["_id"])
+	}
+>>>>>>> 629 configure cors for login UI (#127)
 }
